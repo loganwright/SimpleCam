594a016224a564bb7d16c434d425dbb6a9f8b021
Logan Wright <logan.william.wright@gmail.com>
2014-03-31 11:54:25 -0400

    Initial Commit

---
+++ SimpleCam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleCam
+//
+//  Created by Logan Wright on 3/31/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleCamTests/SimpleCamTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleCamTests.m
+//  SimpleCamTests
+//
+//  Created by Logan Wright on 3/31/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleCamTests : XCTestCase
+
+@end
+
+@implementation SimpleCamTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
