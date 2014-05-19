cc5a6edc46060a25420ccb0ab04c0726a8c5ef93
dkhamsing <danielkhamsing@gmail.com>
2014-05-19 13:18:15 -0700

    Update demo to show simple cam options

--- SimpleCam/RootViewController.m
+++ SimpleCam/RootViewController.m
@@ -8,10 +8,12 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController ()
+@interface RootViewController () <UIActionSheetDelegate>
 
 @property (strong, nonatomic) UIImageView * imgView;
 @property (strong, nonatomic) UILabel * tapLabel;
+@property (nonatomic,strong) SimpleCam *simpleCam;
+@property (nonatomic) BOOL takePhotoImmediately;
 
 @end
 
@@ -31,42 +33,103 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    _tapLabel = [UILabel new];
-    _tapLabel.bounds = CGRectMake(0, 0, 200, 100);
-    _tapLabel.text = @"** TAP TO OPEN **";
-    _tapLabel.textAlignment = NSTextAlignmentCenter;
-    _tapLabel.center = self.view.center;
-    _tapLabel.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
-    [self.view addSubview:_tapLabel];
-    
     _imgView = [UIImageView new];
     _imgView.bounds = CGRectMake(0, 0, 200, 300);
     _imgView.center = self.view.center;
     _imgView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
     _imgView.contentMode = UIViewContentModeScaleAspectFit;
     [self.view addSubview:_imgView];
+
+    _tapLabel = [UILabel new];
+    _tapLabel.bounds = CGRectMake(0, 0, 200, 100);
+    _tapLabel.text = @"TAP TO TAKE PHOTO";
+    _tapLabel.textAlignment = NSTextAlignmentCenter;
+    _tapLabel.center = self.view.center;
+    _tapLabel.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
+    [self.view addSubview:_tapLabel];
     
     UITapGestureRecognizer * tap = [UITapGestureRecognizer new];
     [tap addTarget:self action:@selector(handleTap:)];
     [self.view addGestureRecognizer:tap];
-
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark ACTIONSHEET
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    self.takePhotoImmediately = NO;
+    switch (buttonIndex) {
+        case 0: // default
+        {
+            SimpleCam * simpleCam = [SimpleCam new];
+            simpleCam.delegate= self;
+
+            [self presentViewController:simpleCam animated:YES completion:nil];
+        }
+            break;
+            
+        case 1: // take photo immediately
+        {            
+            self.takePhotoImmediately = YES;
+            
+            SimpleCam * simpleCam = [SimpleCam new];
+            simpleCam.delegate= self;
+            [simpleCam setHideCaptureButton:YES];
+            [simpleCam setHideBackButton:YES];
+            [simpleCam setDisablePhotoPreview:YES];
+            [self presentViewController:simpleCam animated:YES completion:nil];
+        }
+            break;
+            
+        case 2: // overlay
+        {
+            self.simpleCam = [SimpleCam new];
+            self.simpleCam.delegate= self;
+            
+            [self.simpleCam setHideControls:YES];
+            [self.simpleCam setDisablePhotoPreview:YES];
+            
+            CGRect frame;
+            frame.size = CGSizeMake(self.view.frame.size.width, 120);
+            frame.origin.x = 0;
+            frame.origin.y = self.view.frame.size.height -frame.size.height;
+            UIView *overlayView = [[UIView alloc] initWithFrame:frame];
+            overlayView.backgroundColor = [UIColor blackColor];
+            overlayView.alpha = 0.3;
+            
+            UIImage *image = [UIImage imageNamed:@"shutter"];
+            frame.size = image.size;
+            frame.origin.x = (overlayView.frame.size.width -frame.size.width)/2;
+            frame.origin.y = (overlayView.frame.size.height -frame.size.height)/2;
+            UIButton *button = [[UIButton alloc] initWithFrame:frame];
+            [button setImage:image forState:UIControlStateNormal];
+            [button addTarget:self action:@selector(actionPhoto) forControlEvents:UIControlEventTouchUpInside];
+            [overlayView addSubview:button];
+            [self.simpleCam.view addSubview:overlayView];
+            
+            [self presentViewController:self.simpleCam animated:YES completion:nil];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma mark PRIVATE
+
+- (void)actionPhoto
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.simpleCam  capturePhoto];
 }
 
 #pragma mark TAP RECOGNIZER
 
 - (void) handleTap:(UITapGestureRecognizer *)tap {
-    SimpleCam * simpleCam = [SimpleCam new];
-    simpleCam.delegate= self;
-    [simpleCam setHideCaptureButton:YES];
-    [simpleCam setHideBackButton:YES];
-    [simpleCam setDisablePhotoPreview:YES];
-    [self presentViewController:simpleCam animated:YES completion:nil];
+    
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"SimpleCam Options" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Default", @"Take Photo Immediately", @"Custom", nil];
+    [sheet showInView:self.view];
+    
+
 }
 
 #pragma mark SIMPLE CAM DELEGATE
@@ -77,13 +140,13 @@
         // simple cam finished with image
         
         _imgView.image = image;
-        _tapLabel.hidden = YES;
+        //_tapLabel.hidden = NO;
     }
     else {
         // simple cam finished w/o image
         
         _imgView.image = nil;
-        _tapLabel.hidden = NO;
+        //_tapLabel.hidden = NO;
     }
     
     // Close simpleCam - use this as opposed to 'dismissViewController' otherwise, the captureSession may not close properly and may result in memory leaks.
@@ -94,18 +157,9 @@
 
 - (void) simpleCamDidLoadedCamera:(SimpleCam *)simpleCam {
     NSLog(@"Camera loaded ... ");
-    [simpleCam capturePhoto];
+    
+    if (self.takePhotoImmediately)
+        [simpleCam capturePhoto];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
